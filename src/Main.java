fb5243c290bf4dd29361e058861a08f15a676603
iampushkar <m4manishpushkar@gmail.com>
2025-11-15 08:30:49 +0530

    adding ascii

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("🥚 Welcome to Yolked");
         System.out.println("Fun Virtual Egg Incubator");
-        System.out.println("===========================");
-        System.out.println("__   _____  _   _   _     ___  ____  _____ \n" +
-                "\\ \\ / / _ \\| | | | | |   / _ \\/ ___|| ____|\n" +
-                " \\ V / | | | | | | | |  | | | \\___ \\|  _|  \n" +
-                "  | || |_| | |_| | | |__| |_| |___) | |___ \n" +
-                "  |_| \\___/ \\___/  |_____\\___/|____/|_____|");
+        var ascii = """
+                __     ______  _    _   _      ____   _____ ______\s
+                \\ \\   / / __ \\| |  | | | |    / __ \\ / ____|  ____|
+                 \\ \\_/ / |  | | |  | | | |   | |  | | (___ | |__  \s
+                  \\   /| |  | | |  | | | |   | |  | |\\___ \\|  __| \s
+                   | | | |__| | |__| | | |___| |__| |____) | |____\s
+                   |_|  \\____/ \\____/  |______\\____/|_____/|______|                                                 \s
+                """;
+        System.out.println(ascii);
 
         EggIncubator incubator = new EggIncubator();
         incubator.start();
-
     }
 }
